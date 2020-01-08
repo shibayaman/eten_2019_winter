069b8cdcd7ed3e7d900c8a78aa27d1087604cec1
unknown <2180418@s2180418.maetel.ecc.ac.jp>
2020-01-08 12:29:30 +0900

    create registration form

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -10,120 +10,204 @@
     </head>
     <body>
 
+
+      <div class="column is-three-fifths is-offset-5">
+          <h1 class="title">作品登録フォーム</h1>
+      </div>
+      <br>
+      <div class="columns">
+        <div class="column is-offset-3">
+          <label class="label is-large">作品コード : I05</label>
+        </div>
+        <div class="column">
+          <label class="label is-large">クラス名 : IE2A</label>
+        </div>
+      </div>
+
+      <div id="attention-field"></div>
+
       <form action="confirmation.blade.php" method="post">
         <div class="section">
-            <label class="label">作品名</label>
-            <div class="field">
-                <div class="control">
-                  <input class="input column is-half" type="text" placeholder="作品名を入力してください">
+
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">※作品名</label>
+              </div>
+              <div class="field-body">
+                <div class="field" id="titlefield">
+                  <div class="control">
+                    <input class="input column is-two-thirds" type="text" id="title" name="title" placeholder="作品名を入力してください">
+                  </div>
                 </div>
+              </div>
             </div>
 
-            <label class="label">キャッチコピー</label>
-              <div class="field">
-                <div class="control">
-                  <input class="input column is-half" type="text" placeholder="30字以内" maxlength="30">
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">※キャッチコピー</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                      <input class="input column is-two-thirds" type="text" id="catch_copy" placeholder="30字以内でキャッチコピーを書いてください" maxlength="30">
+                    </div>
+                  </div>
                 </div>
               </div>
 
-            <label class="label">詳細</label>
-              <div class="field">
-                <textarea class="textarea column is-two-thirds" placeholder="作品の詳細を書いてください" maxlength="300"></textarea>
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">※詳細</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                      <textarea class="textarea" placeholder="作品の詳細を書いてください" id="detail" maxlength="300" rows="5"></textarea>
+                    </div>
+                  </div>
+                </div>
               </div>
 
-            <label class="label">画像</label>
-              <div class="field">
-                <div id="file-selector" class="file has-name">
-                  <label class="file-label">
-                    <input class="file-input" type="file" name="resume">
-                    <span class="file-cta">
-                      <span class="file-icon">
-                        <i class="fas fa-upload"></i>
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">※画像</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div id="file-selector" class="file has-name">
+                    <label class="file-label">
+                      <input class="file-input" type="file" id="image" name="image" accept="image/*">
+                      <span class="file-cta">
+                        <span class="file-icon">
+                          <i class="fas fa-upload"></i>
+                        </span>
+                        <span class="file-label">
+                          ファイルを選択
+                        </span>
                       </span>
-                      <span class="file-label">
-                        ファイルを選択
+                      <span class="file-name" id="file-name">
+                        選択されていません
                       </span>
-                    </span>
-                    <span class="file-name">
-                      選択されていません
-                    </span>
-                  </label>
+                    </label>
+                  </div>
                 </div>
+              </div>
             </div>
 
-            <label class="label">制作期間</label>
-                <div class="field has-addons">
-                  <p class="control">
-                    <input class="input" type="number" placeholder="1" min="1">
-                  </p>
-                  <p class="control">
-                    <span class="select">
-                      <select>
-                        <option>時間</option>
-                        <option>週</option>
-                        <option>月</option>
+                <div class="field is-horizontal">
+                  <div class="field-label is-normal">
+                    <label class="label">※制作期間</label>
+                  </div>
+                  <div class="field-body">
+                    <div class="field has-addons">
+                      <p class="control">
+                        <input class="input" type="number"  id="period" name="period" placeholder="0" min="1">
+                      </p>
+                      <p class="control">
+                        <span class="select">
+                          <select>
+                            <option>時間</option>
+                            <option>週間</option>
+                            <option>ヶ月</option>
+                          </select>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">※代表者名</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control has-icons-left has-icons-right">
+                      <input class="input column is-one-third" type="text" id="represent" name="represent" placeholder="代表者の名前を入力してください">
+                      <span class="icon is-small is-left">
+                        <i class="fas fa-user"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">※チーム名</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <input class="input column is-one-third" type="text" id="team" name="team" placeholder="チーム名を入力してください">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="field" id="memberinput">
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">メンバー</label>
+              </div>
+              <div class="field-body">
+
+                  <div class="field">
+                    <div class="control has-icons-left has-icons-right">
+                      <input class="input column is-one-third" type="text" name="member[]" placeholder="代表者以外のメンバーの名前を入力してください">
+                      <span class="icon is-small is-left">
+                        <i class="fas fa-user"></i>
+                      </span>
+                    </div>
+                  </div>
+
+              </div>
+            </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <button class="button" type="button" id="add_member">追加</button>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">ジャンル</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <div class="select">
+                      <select id="genre" name="genre">
+                        <option>モバイルアプリ</option>
+                        <option>PCアプリケーション</option>
+                        <option>ゲーム</option>
+                        <option>その他</option>
                       </select>
-                    </span>
-                  </p>
-                </div>
-
-            <label class="label">代表者名</label>
-              <div class="field">
-                <div class="control has-icons-left has-icons-right">
-                  <input class="input column is-one-third" type="text" placeholder="代表者の名前を入力してください">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-user"></i>
-                  </span>
-                </div>
-              </div>
-
-            <label class="label">チーム名</label>
-            <div class="field">
-                <div class="control">
-                  <input class="input column is-one-third" type="text" placeholder="チーム名を入力してください">
-                </div>
-            </div>
-
-            <label class="label">メンバー</label>
-            <div class="field">
-              <div class="control has-icons-left has-icons-right">
-                <input class="input column is-one-third" type="text" placeholder="メンバーの名前を入力してください">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-user"></i>
-                </span>
-              </div>
-            </div>
-            <div class="field">
-              <button class="button" onClick="add_member">追加</button>
-            </div>
-
-            <label class="label">クラス名</label>
-            <div class="field">
-                <div class="control">
-                  <div class="select">
-                    <select>
-                      <option>IE1A</option>
-                      <option>IE2A</option>
-                    </select>
+                    </div>
                   </div>
                 </div>
+              </div>
+            </div>
+            <div class="field" id="genrefield">
             </div>
 
-            <label class="label">ジャンル</label>
-            <div class="field">
-                <div class="control">
-                  <div class="select">
-                    <select>
-                      <option>モバイルアプリ</option>
-                      <option>PCアプリケーション</option>
-                      <option>ゲーム</option>
-                      <option>その他</option>
-                    </select>
+
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                      <input class="button is-info" type="submit" id="submit" value="確認">
                   </div>
                 </div>
-            </div>
-            <div class="control">
-                <input class="button is-info" type="submit" value="確認">
+              </div>
             </div>
         </div>
       </form>
