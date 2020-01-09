da3e13dd92f91d7dddf2c3cc71a650b2d5c6d170
macoto5636 <maakitak@icloud.com>
2020-01-09 17:45:10 +0900

    database

--- database/migrations/2019_12_31_113608_create_tokens_table.php
+++ database/migrations/2019_12_31_113608_create_tokens_table.php
@@ -16,11 +16,11 @@ class CreateTokensTable extends Migration
         Schema::create('tokens', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('token', 64)->unique();
-            $table->string('project_code', 20)->unique();
+            $table->string('project_code', 20);
             $table->unsignedTinyInteger('season_id');
             $table->string('class_id',10);
             $table->timestamp('expires_at');
-            $table->unique(['project_code', 'season_id']);
+            $table->unique(['project_code']);
             $table->timestamps();
             //今回2038年問題は対応しません(18年も代々使われるシステムを作るつもりはないです)
 
