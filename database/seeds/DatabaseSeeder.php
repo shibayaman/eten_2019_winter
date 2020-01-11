7ef4af109fdc81d3827a361ccb4dede9c3943021
shibayaman <shibayaman7374@gmail.com>
2020-01-12 01:13:31 +0900

    Create admin seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ClassesTableSeeder::class,
             SeasonsTableSeeder::class,
+            AdminTableSeeder::class,
         ]);
     }
 }
