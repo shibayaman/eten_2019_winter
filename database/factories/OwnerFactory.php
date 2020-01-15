a617022165735dfeb9c112dde4f07f03a6f14274
shibayaman <shibayaman7374@gmail.com>
2020-01-15 15:44:20 +0900

    Fix project factory

--- database/factories/OwnerFactory.php
+++ database/factories/OwnerFactory.php
@@ -9,7 +9,7 @@ $factory->define(Owner::class, function (Faker $faker) {
     $randomInt = random_int(0, 2);
     return [
         'password' => Str::random(16),
-        'project_code' => ['W', 'I', 'G'][$randomInt] . $faker->unique()->regexify('[0-9]{3}'),
+        'project_code' => ['W', 'I', 'G'][$randomInt] . $faker->unique()->regexify('[0-9]{2}'),
         'season_id' => 1,
         'class_id' => ['WD1A', 'IE1A', 'CD2A'][$randomInt],
         'expires_at' => new DateTime(Config::get('const.eventDate'))
