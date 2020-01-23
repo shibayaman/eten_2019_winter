1fb33bf56f093062bcee1dcfaaff188f910babc1
Akisu1016 <akisu1016@gmail.com>
2020-01-23 14:49:02 +0900

    edit

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,16 +6,26 @@
 <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 @endsection
 
-@section('content')
-<h1 class="title">ユーザ管理画面</h1>
-<a href="{{ route('owners.create') }}">ユーザー作成</a><br>
-<a href="{{ route('owners.index') }}">全ユーザー一覧</a> |
-<a href="{{ route('owners.index', ['field' => $fields['IT']]) }}">IT科ユーザー一覧</a> |
-<a href="{{ route('owners.index', ['field' => $fields['WEB']]) }}">Web科ユーザー一覧</a> |
-<a href="{{ route('owners.index', ['field' => $fields['GRAPHIC']]) }}">グラフィック科ユーザー一覧</a>
+<body>
+	@section('content')
+	<div class="field column is-offset-10">
+		<form action="{{ route('logout') }}" method="post">
+			@csrf
+			<input type="submit" value="ログアウト" class="button is-rounded is-dark is-medium">
+		</form>
+	</div>
+	<div class="field">
+		<div class="column is-three-fifths is-offset-5">
+				<h1 class="title">ユーザ管理画面</h1>
+		</div>
+	</div>
+	<hr>
 
-<form action="{{ route('admin.logout') }}" method="post">	
-	@csrf
-  <input type="submit" class="button" value="ログアウト">
-</form>
+	<a class="button" href="{{ route('owners.create') }}">ユーザー作成</a><br>
+	<a href="{{ route('owners.index') }}">全ユーザー一覧</a> |
+	<a href="{{ route('owners.index', ['field' => $fields['IT']]) }}">IT科ユーザー一覧</a> |
+	<a href="{{ route('owners.index', ['field' => $fields['WEB']]) }}">Web科ユーザー一覧</a> |
+	<a href="{{ route('owners.index', ['field' => $fields['GRAPHIC']]) }}">グラフィック科ユーザー一覧</a>
+
 @endsection
+</body>
